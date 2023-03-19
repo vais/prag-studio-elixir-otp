8b994e40043c31df06c577dbef3f9f20190d93e6
Vais Salikhov <vsalikhov@gmail.com>
2023-03-18 23:26:06 -0400

    Change HttpServerTest to send concurrent requests

--- test/servy/http_server_test.exs
+++ test/servy/http_server_test.exs
@@ -6,9 +6,29 @@ defmodule Servy.HttpServerTest do
   test "the server" do
     spawn(HttpServer, :start, [4000])
 
-    {:ok, response} = HTTPoison.get("http://localhost:4000/wildthings")
+    parent = self()
 
-    assert response.status_code == 200
-    assert response.body == "Bears, Lions, Tigers"
+    bears = %{
+      1 => "Teddy",
+      2 => "Smokey",
+      3 => "Paddington",
+      4 => "Scarface",
+      5 => "Snow"
+    }
+
+    for id <- Map.keys(bears) do
+      spawn(fn ->
+        {:ok, response} = HTTPoison.get("http://localhost:4000/bears/#{id}")
+        send(parent, {:response, id, response})
+      end)
+    end
+
+    for _ <- bears do
+      receive do
+        {:response, id, response} ->
+          assert response.status_code == 200
+          assert response.body =~ bears[id]
+      end
+    end
   end
 end
