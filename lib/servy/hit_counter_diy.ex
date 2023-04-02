bbede5312a96645d30c4417683a61e7d5b2b171e
Vais Salikhov <vsalikhov@gmail.com>
2023-04-01 22:26:25 -0400

    Refactor HitCounterDiy to GenServer

--- lib/servy/hit_counter_diy.ex
+++ lib/servy/hit_counter_diy.ex
@@ -1,89 +1,52 @@
-defmodule Servy.HitCounterDiy.GenericServer do
-  def start(mod, %{} = state) do
-    maybe_start(mod, state, Process.whereis(mod))
-  end
-
-  defp maybe_start(mod, state, nil) do
-    pid = spawn(fn -> loop(mod, state) end)
-    Process.register(pid, mod)
-    pid
-  end
-
-  defp maybe_start(_mod, _state, pid), do: pid
-
-  defp loop(mod, state) do
-    receive do
-      {:cast, message} ->
-        new_state = mod.handle_cast(message, state)
-        loop(mod, new_state)
-
-      {:call, caller, message} ->
-        {response, new_state} = mod.handle_call(message, state)
-        send(caller, {:response, response})
-        loop(mod, new_state)
-
-      other ->
-        new_state = mod.handle_info(other, state)
-        loop(mod, new_state)
-    end
-  end
-
-  def cast(pid, message) do
-    send(pid, {:cast, message})
-  end
-
-  def call(pid, message) do
-    send(pid, {:call, self(), message})
-
-    receive do
-      {:response, response} -> response
-    end
-  end
-end
-
 defmodule Servy.HitCounterDiy do
+  use GenServer
   require Logger
 
-  alias Servy.HitCounterDiy.GenericServer
-
   def start(%{} = state) do
-    GenericServer.start(__MODULE__, state)
+    case GenServer.start(__MODULE__, state, name: __MODULE__) do
+      {:ok, pid} -> pid
+      {:error, {:already_started, pid}} -> pid
+    end
+  end
+
+  def init(state) do
+    {:ok, state}
   end
 
   def handle_info(other, state) do
     Logger.error("Unexpected message: #{inspect(other)}")
-    state
+    {:noreply, state}
   end
 
   def handle_cast(:reset, _state) do
-    %{}
+    {:noreply, %{}}
   end
 
   def handle_cast({:hit, path}, state) do
-    Map.update(state, path, 1, &(&1 + 1))
+    {:noreply, Map.update(state, path, 1, &(&1 + 1))}
   end
 
-  def handle_call({:get_count, path}, state) do
-    {Map.get(state, path, 0), state}
+  def handle_call({:get_count, path}, _from, state) do
+    {:reply, Map.get(state, path, 0), state}
   end
 
-  def handle_call(:report, state) do
-    {state, state}
+  def handle_call(:report, _from, state) do
+    {:reply, state, state}
   end
 
   def reset() do
-    GenericServer.cast(__MODULE__, :reset)
+    GenServer.cast(__MODULE__, :reset)
   end
 
   def hit(path) do
-    GenericServer.cast(__MODULE__, {:hit, path})
+    GenServer.cast(__MODULE__, {:hit, path})
   end
 
   def get_count(path) do
-    GenericServer.call(__MODULE__, {:get_count, path})
+    GenServer.call(__MODULE__, {:get_count, path})
   end
 
   def report do
-    GenericServer.call(__MODULE__, :report)
+    GenServer.call(__MODULE__, :report)
   end
 end
