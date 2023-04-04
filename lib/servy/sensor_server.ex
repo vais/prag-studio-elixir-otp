6d60a59582319fb7a28e24049bbcbd9d184edf6d
Vais Salikhov <vsalikhov@gmail.com>
2023-04-03 22:06:48 -0400

    Make refresh interval dynamic

--- lib/servy/sensor_server.ex
+++ lib/servy/sensor_server.ex
@@ -1,34 +1,45 @@
 defmodule Servy.SensorServer do
+  defstruct sensor_data: %{}, refresh_interval: :timer.seconds(5)
+
   use GenServer
 
   alias Servy.{Tracker, VideoCam}
 
   def start() do
-    GenServer.start(__MODULE__, nil, name: __MODULE__)
+    GenServer.start(__MODULE__, %__MODULE__{}, name: __MODULE__)
   end
 
   def get_sensor_data do
     GenServer.call(__MODULE__, :get_sensor_data)
   end
 
-  def init(_state) do
-    initial_state = fetch_sensor_data()
-    schedule_refresh()
-    {:ok, initial_state}
+  def set_refresh_interval(refresh_interval) do
+    GenServer.cast(__MODULE__, {:set_refresh_interval, refresh_interval})
+  end
+
+  def init(%__MODULE__{} = state) do
+    sensor_data = fetch_sensor_data()
+    schedule_refresh(state.refresh_interval)
+    {:ok, %{state | sensor_data: sensor_data}}
   end
 
   def handle_call(:get_sensor_data, _from, state) do
-    {:reply, state, state}
+    {:reply, state.sensor_data, state}
   end
 
-  def handle_info(:refresh, _state) do
-    new_state = fetch_sensor_data()
-    schedule_refresh()
-    {:noreply, new_state}
+  def handle_cast({:set_refresh_interval, refresh_interval}, state) do
+    {:noreply, %{state | refresh_interval: refresh_interval}}
   end
 
-  defp schedule_refresh() do
-    Process.send_after(self(), :refresh, :timer.seconds(5))
+  def handle_info(:refresh, state) do
+    # IO.puts("#{Time.utc_now()} refreshing")
+    sensor_data = fetch_sensor_data()
+    schedule_refresh(state.refresh_interval)
+    {:noreply, %{state | sensor_data: sensor_data}}
+  end
+
+  defp schedule_refresh(refresh_interval) do
+    Process.send_after(self(), :refresh, refresh_interval)
   end
 
   defp fetch_sensor_data do
