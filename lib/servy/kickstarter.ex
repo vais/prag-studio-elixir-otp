0a737336bd5440484ed3a958458f06452de23bdb
Vais Salikhov <vsalikhov@gmail.com>
2023-04-06 22:02:11 -0400

    Write a genserver to babysit the http server

---
+++ lib/servy/kickstarter.ex
@@ -0,0 +1,30 @@
+defmodule Servy.Kickstarter do
+  use GenServer
+
+  def start do
+    GenServer.start(__MODULE__, :ok, name: __MODULE__)
+  end
+
+  def get_http_server_pid do
+    GenServer.call(__MODULE__, :get_http_server_pid)
+  end
+
+  def init(:ok) do
+    Process.flag(:trap_exit, true)
+    http_server_pid = start_http_server()
+    {:ok, http_server_pid}
+  end
+
+  def handle_info({:EXIT, _http_server_pid = state, _reason}, state) do
+    http_server_pid = start_http_server()
+    {:noreply, http_server_pid}
+  end
+
+  def handle_call(:get_http_server_pid, _from, state) do
+    {:reply, state, state}
+  end
+
+  defp start_http_server() do
+    spawn_link(Servy.HttpServer, :start, [4000])
+  end
+end
